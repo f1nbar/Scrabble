a6017d698920367767a112b1bdb4573cd2f56959
Finbar <finbar@pop-os.localdomain>
2020-02-07 13:50:29 +0000

    renamed blank tiles as underscore for readability

--- scrabble/Main.java
+++ scrabble/Main.java
@@ -1,28 +1,24 @@
 package scrabble;
 
-import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System. in);
+		Scanner input = new Scanner(System.in);
 		System.out.print("Enter your name: ");
 		String name = input.next();
 		input.close();
-	
+
 		Pool pool = new Pool();
 		Player player = new Player(name);
 		Frame frame = new Frame();
-		
+
 		pool.fillPool();
-	
-	    while(frame.frameSize() < 7) {
-	    	frame.fillFrame(pool.randomTile());
-	    }
-		
-		
+
+		while (frame.frameSize() < 7) {
+			frame.fillFrame(pool.randomTile());
+		}
 		System.out.println("Your frame is: " + frame.toString());
 	}
-
 }
