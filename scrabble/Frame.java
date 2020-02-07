3657403c01df5cfb85c2a11bc725500316d64fff
ConorHK <conorknowles@gmail.com>
2020-02-07 12:03:27 +0000

    Adding intro javadoc comment

--- scrabble/Frame.java
+++ scrabble/Frame.java
@@ -2,6 +2,12 @@ package scrabble;
 
 import java.util.LinkedList;
 
+/**
+ * Frame class which stores and allows manipulation of Player's frames.
+ * 
+ * @author Peter O'Donnell
+ *
+ */
 public class Frame {
 
 	private int FRAME_SIZE = 7;
@@ -9,7 +15,6 @@ public class Frame {
 	LinkedList<Tile> frame = new LinkedList<Tile>(); // linked list to store tile objects
 
 	public void removeLetter(Tile... c) { // This function removes the tiles from the frame
-
 		for (int i = 0; i < c.length; i++) {
 			frame.remove(c[i]);
 			System.out.println(toString());
@@ -18,7 +23,6 @@ public class Frame {
 
 	public boolean checkLetters(Tile... c) { // This checks if the letters are avalaible or not returning a suitable
 												// boolean
-
 		for (int i = 0; i < c.length; i++) {
 			if (frame.indexOf(c[i]) == -1) {
 				return false;
@@ -28,19 +32,15 @@ public class Frame {
 	}
 
 	public boolean isEmpty() { // returns true if frame is empty
-
 		if (frame.isEmpty()) {
 			return true;
 		}
 		return false;
-
 	}
 
 	@Override
 	public String toString() { // returns the string
-
 		String result = null;
-
 		for (int i = 0; i < frame.size(); i++) {
 			if (result == null) {
 				result = frame.get(i).getLetter();
@@ -48,7 +48,6 @@ public class Frame {
 				result = result + frame.get(i).getLetter();
 			}
 		}
-
 		return result;
 	}
 
@@ -58,12 +57,10 @@ public class Frame {
 			for (int i = 0; i < t.length; i++) {
 				frame.add(t[i]);
 			}
-
 			return true;
 		} else {
 			return false;
 		}
-
 	}
 
 	public Tile getTile(int i) { // returns letter in index i
@@ -72,7 +69,6 @@ public class Frame {
 		} else {
 			throw new IllegalArgumentException("INDEX OUT OF BOUNDS:getTile");
 		}
-
 	}
 
 }
