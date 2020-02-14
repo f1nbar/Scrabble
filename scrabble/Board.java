42070b87d8ba356bb185e2785260cbce002f4582
Finbar <finbar@pop-os.localdomain>
2020-02-14 10:35:22 +0000

    Added empty methods for numTiles, resetBoard and displayBoard and jUnit testing for the reset method

--- scrabble/Board.java
+++ scrabble/Board.java
@@ -1,5 +1,25 @@
 package scrabble;
 
 public class Board {
+	
+	public void resetBoard() {
+		
+	}
+	
+	public void displayBoard() {
+		
+	}
+	
+	public void placeTile() {
+		
+	}
+	
+	public int numTiles() {
+		int num = 0;
+		
+		return num ;
+	}
+	
+
 
 }
--- scrabble/BoardTest.java
+++ scrabble/BoardTest.java
@@ -1,5 +1,26 @@
 package scrabble;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
 public class BoardTest {
+	
+	Board board = new Board();
+	
+	@Test
+	public void testResetBoard() {
+		for (int i = 0; i < 10; i++) {
+			board.placeTile();
+		} 
+		board.resetBoard();
+		assertEquals(0, board.numTiles());
+	}
+	
+	@Test
+	public void testDisplayBoard() {
+		         
+	}
+	
 
 }
