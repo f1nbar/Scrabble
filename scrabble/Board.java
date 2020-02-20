312617bee841eb75fec6ecd2385a9850cb57d55e
Finbar <finbar@pop-os.localdomain>
2020-02-20 15:37:06 +0000

    Added Triple word, Dobule letter and star in centre of board, changed board to String array

--- scrabble/Board.java
+++ scrabble/Board.java
@@ -2,34 +2,60 @@ package scrabble;
 
 public class Board {
 
-	char scrabbleBoard[][] = new char [15][15]; //temp 2d array for board drawing implementation
+	static String scrabbleBoard[][] = new String [15][15]; //temp 2d array for board drawing implementation
 	
 	
+	public void fillBoard() {
 
-	public void resetBoard() {
+		for (int i = 0; i < scrabbleBoard.length; i++) {
+			for (int j = 0; j < scrabbleBoard.length; j++) {
+				scrabbleBoard[i][j] = "  ";
+			}
+		}
+        //Triple Words
+		scrabbleBoard[0][0] = "TW";
+		scrabbleBoard[0][14] = "TW";
+		scrabbleBoard[14][14] = "TW";
+		scrabbleBoard[14][0] = "TW";
+		scrabbleBoard[0][7] = "TW";
+		scrabbleBoard[7][0] = "TW";
+		
+		scrabbleBoard[7][7] = "*"; //Star in centre
+		
+		//Double Letters
+		scrabbleBoard[0][3] = "DL";
+		scrabbleBoard[0][11] = "DL";
+		scrabbleBoard[14][3] = "DL";
+		scrabbleBoard[14][11] = "DL";
+		scrabbleBoard[3][14] = "DL";
+		scrabbleBoard[11][14] = "DL";
+		scrabbleBoard[3][0] = "DL";
+		scrabbleBoard[11][0] = "DL";
+	
 
+		
+
+
+		
+		
+	
 	}
 
-	public void displayBoard() {
-		
-       //test code for displaying board
-		for (int i = 0; i < 15; i++) {
-			for (int j = 0; j < 15; j++) {
-			   scrabbleBoard[i][j] = 'x';
-				
-			}
-			
-		}
 
-	
+	public void resetBoard() {
+       //TODO Call function to fill board
+	}
+
+	public void displayBoard(String board[][]) {	
+    
 		System.out.println("   _________________________________________________________________________________________   ");				
       for (int i = 14; i>= 0; i--) {
 
 		for(int y = 0; y<15; y++)
 		{
 		System.out.print("  |" );   
-		System.out.print(scrabbleBoard[i][y]);
-		System.out.print("  ");
+		System.out.print(board[i][y]); 
+		System.out.print(" ");
 		}   
 		System.out.println();
         System.out.println("  |_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|  ");
@@ -49,7 +75,8 @@ public class Board {
 
 	public static void main(String[] args) {
 		Board board = new Board();
-		board.displayBoard();
+		board.fillBoard();
+		board.displayBoard(scrabbleBoard);
 
 
 }
