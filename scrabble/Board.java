06b5b4f698ed247021db3f8cbd08b07a8c0725d1
Finbar <finbar@pop-os.localdomain>
2020-02-21 10:33:55 +0000

    display board method modified in order to print hashmap instead of 2d array

--- scrabble/Board.java
+++ scrabble/Board.java
@@ -1,9 +1,93 @@
 package scrabble;
 
+import java.util.HashMap;
+
 public class Board {
 
-	static String scrabbleBoard[][] = new String [15][15]; //temp 2d array for board drawing implementation
-	
+
+	private static HashMap<Integer, String> tilePoints;
+
+	public Board(){
+		initBoardScores();
+	}
+	private void initBoardScores() {
+        Board.tilePoints = new HashMap<Integer, String>();
+        //Triple word
+        tilePoints.put(00, "TW");
+        tilePoints.put(70, "TW");
+        tilePoints.put(07, "TW");
+        tilePoints.put(14, "TW");
+        tilePoints.put(140, "TW");
+        tilePoints.put(147, "TW");
+        tilePoints.put(714, "TW");
+        tilePoints.put(1414, "TW");
+
+        //Double letter
+        tilePoints.put(51, "TL");
+        tilePoints.put(91, "TL");
+        tilePoints.put(15, "TL");
+        tilePoints.put(55, "TL");
+        tilePoints.put(95, "TL");
+        tilePoints.put(135, "TL");
+        tilePoints.put(19, "TL");
+        tilePoints.put(59, "TL");
+        tilePoints.put(99, "TL");
+        tilePoints.put(139, "TL");
+        tilePoints.put(513, "TL");
+        tilePoints.put(913, "TL");
+
+        //Double word
+        tilePoints.put(11, "DW");
+        tilePoints.put(22, "DW");
+        tilePoints.put(33, "DW");
+        tilePoints.put(44, "DW");
+        tilePoints.put(113, "DW");
+        tilePoints.put(212, "DW");
+        tilePoints.put(311, "DW");
+        tilePoints.put(410, "DW");
+        tilePoints.put(131, "DW");
+        tilePoints.put(122, "DW");
+        tilePoints.put(113, "DW");
+        tilePoints.put(104, "DW");
+        tilePoints.put(1010, "DW");
+        tilePoints.put(1111, "DW");
+        tilePoints.put(1212, "DW");
+        tilePoints.put(1313, "DW");
+
+        //Double letter
+        tilePoints.put(30, "DL");
+        tilePoints.put(110, "DL");
+        tilePoints.put(62, "DL");
+        tilePoints.put(82, "DL");
+        tilePoints.put(03, "DL");
+        tilePoints.put(73, "DL");
+        tilePoints.put(143, "DL");
+        tilePoints.put(26, "DL");
+        tilePoints.put(66, "DL");
+        tilePoints.put(86, "DL");
+        tilePoints.put(126, "DL");
+        tilePoints.put(37, "DL");
+        tilePoints.put(117, "DL");
+        tilePoints.put(28, "DL");
+        tilePoints.put(68, "DL");
+        tilePoints.put(88, "DL");
+        tilePoints.put(128, "DL");
+        tilePoints.put(011, "DL");
+        tilePoints.put(711, "DL");
+        tilePoints.put(1411, "DL");
+        tilePoints.put(612, "DL");
+        tilePoints.put(812, "DL");
+        tilePoints.put(314, "DL");
+        tilePoints.put(1114, "DL");
+    }
+
+    public static String getSquareValue(int location) {
+        if (Board.tilePoints.containsKey(location)) {
+            return Board.tilePoints.get(location);
+        } else {
+            return null;
+        }
+    }	
 	
 
 
@@ -11,16 +95,23 @@ public class Board {
        initBoardScores();
 	}
 
-	public void displayBoard(String board[][]) {	
+	public void displayBoard() {	
     
+		System.out.println("                                           SCRABBLE                                            ");
 		System.out.println("   _________________________________________________________________________________________   ");				
       for (int i = 14; i>= 0; i--) {
 
 		for(int y = 0; y<15; y++)
 		{
+		int position = concatInt(i, y); 
 		System.out.print("  |" );   
-		System.out.print(board[i][y]); 
-		System.out.print(" ");
+		if(tilePoints.get(position) == null) {
+			System.out.print("  ");
+		}
+		else {
+		System.out.print(tilePoints.get(position)); 
+		}
+		System.out.print(" "); 
 		}   
 		System.out.println();
         System.out.println("  |_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____|  ");
@@ -36,6 +127,17 @@ public class Board {
 		int num = 0;
 
 		return num;
+	}
+	
+	public static void main(String[] args) {
+		Board board = new Board();
+		board.displayBoard();
+	}
+	
+	public int concatInt(int a,int b) {
+		String s = "" + a + b;
+		int pos = Integer.parseInt(s);
+		return pos;
 	}
 
 
